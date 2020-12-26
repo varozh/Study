6d8307c7e3db1353411f53abb69ade0466827e34
Andrey Vorozheykin <varozh73@gmail.com>
2020-12-27 01:01:30 +0400

    commit in home 26.12

--- src/other/tasks/Fibonachi.java
+++ src/other/tasks/Fibonachi.java
@@ -7,6 +7,16 @@ import java.sql.SQLOutput;
 public class Fibonachi {
 
     public void run() {
+        test1();
+    }
+
+    private void test1() {
+        String input = "camelCasing";
+        String result = input.replaceAll("(?=[A-Z])", "$0 ");
+        System.out.println(result);
+    }
+
+    private void fibonachi() {
         System.out.println("Введите количество значений: ");
         int n = Labs.num.nextInt();
         int x = 0, y = 0, z = 1;
